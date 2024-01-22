df634610acfa37f21d975aad0651021c9aa158aa
Richenel2 <yvanprogramme@gmail.com>
2024-01-22 11:08:01 +0100

    hiiiiii

--- ai.py
+++ ai.py
@@ -5,76 +5,76 @@ from linkedIn import post_linkedin
 from typing_extensions import Annotated
 import json
 import requests
-
-config_list = autogen.config_list_from_json(
-    "OAI_CONFIG_LIST",
-    filter_dict={
-        "model": ["gpt-4-1106-preview"],
-    },
-)
-
-llm_config = {
-    "config_list": config_list,
-    "timeout": 120,
-}
-chatbot = autogen.AssistantAgent(
-    name="chatbot",
-    system_message="You are an expert in managing the linkedin page of Richenel's AI Agency, a company that automates processes via AI for its clients. In order to get more customers and attract subscribers, you need to write a linkendin post with emoji that is interesting to read for an average user and that will encourage them to contact us for our services. You will choose a new random topic that is different to the topics that have already been used but it must be interesting and easy to read for the user with very few complex terms. then you will register the newly chosen topic using the given function. you will also have to generate an image via the given function describing in detail the image that will be the most captivating for the chosen topic and also sending the post with emoji that was generated to the function. reply with TERMINATE when you have finished.",
-    llm_config=llm_config,
-)
-
-# create a UserProxyAgent instance named "user_proxy"
-user_proxy = autogen.UserProxyAgent(
-    name="user_proxy",
-    is_termination_msg=lambda x: x.get("content", "") and x.get("content", "").rstrip().endswith("TERMINATE"),
-    human_input_mode="NEVER",
-    max_consecutive_auto_reply=3,
-)
-
-@user_proxy.register_for_execution()
-@chatbot.register_for_llm(description="generate image an post the massa")
-def generate_images_and_post(
-        linkindIn_post: Annotated[str, "the complete linkedin post"],
-        description: Annotated[str, "hyper detailed description of the image that you want"],
-)-> str:
-    client = OpenAI(api_key = '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')
-
-    response = client.images.generate(
-        model="dall-e-3",
-        prompt=f'{description}',
-        size='1024x1024',
-        n=1
-    )
-    img_data = requests.get(response.data[0].url).content
-    post_linkedin(message=linkindIn_post,image=img_data)
-
-    return 'Done'
-
-
-@user_proxy.register_for_execution()
-@chatbot.register_for_llm(description="save the new topic")
-def save_topic(
-
-        topic: Annotated[str, "the new topic to save"],
-)-> str:
-    with open('subject.json', 'r') as f:
-        data = json.load(f)
-        data.append(topic)
-        if len(data)>20:
-            data.pop(0)
-    with open('subject.json', 'w') as json_file:
-        json.dump(data, json_file)
-    return "done"
-
-@user_proxy.register_for_execution()
-@chatbot.register_for_llm(description="get previous topic")
-def previous_topic(
-)-> list:
-    with open('subject.json', 'r') as f:
-        data = json.load(f)
-    return data
-
 def execute():
+    config_list = autogen.config_list_from_json(
+        "OAI_CONFIG_LIST",
+        filter_dict={
+            "model": ["gpt-4-1106-preview"],
+        },
+    )
+
+    llm_config = {
+        "config_list": config_list,
+        "timeout": 120,
+    }
+    chatbot = autogen.AssistantAgent(
+        name="chatbot",
+        system_message="You are an expert in managing the linkedin page of Richenel's AI Agency, a company that automates processes via AI for its clients. In order to get more customers and attract subscribers, you need to write a linkendin post with emoji that is interesting to read for an average user and that will encourage them to contact us for our services. You will choose a new random topic that is different to the topics that have already been used but it must be interesting and easy to read for the user with very few complex terms. then you will register the newly chosen topic using the given function. you will also have to generate an image via the given function describing in detail the image that will be the most captivating for the chosen topic and also sending the post with emoji that was generated to the function. reply with TERMINATE when you have finished.",
+        llm_config=llm_config,
+    )
+
+    # create a UserProxyAgent instance named "user_proxy"
+    user_proxy = autogen.UserProxyAgent(
+        name="user_proxy",
+        is_termination_msg=lambda x: x.get("content", "") and x.get("content", "").rstrip().endswith("TERMINATE"),
+        human_input_mode="NEVER",
+        max_consecutive_auto_reply=3,
+    )
+
+    @user_proxy.register_for_execution()
+    @chatbot.register_for_llm(description="generate image an post the massa")
+    def generate_images_and_post(
+            linkindIn_post: Annotated[str, "the complete linkedin post"],
+            description: Annotated[str, "hyper detailed description of the image that you want"],
+    )-> str:
+        client = OpenAI(api_key = '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')
+
+        response = client.images.generate(
+            model="dall-e-3",
+            prompt=f'{description}',
+            size='1024x1024',
+            n=1
+        )
+        img_data = requests.get(response.data[0].url).content
+        post_linkedin(message=linkindIn_post,image=img_data)
+
+        return 'Done'
+
+
+    @user_proxy.register_for_execution()
+    @chatbot.register_for_llm(description="save the new topic")
+    def save_topic(
+
+            topic: Annotated[str, "the new topic to save"],
+    )-> str:
+        with open('subject.json', 'r') as f:
+            data = json.load(f)
+            data.append(topic)
+            if len(data)>20:
+                data.pop(0)
+        with open('subject.json', 'w') as json_file:
+            json.dump(data, json_file)
+        return "done"
+
+    @user_proxy.register_for_execution()
+    @chatbot.register_for_llm(description="get previous topic")
+    def previous_topic(
+    )-> list:
+        with open('subject.json', 'r') as f:
+            data = json.load(f)
+        return data
+
+
     user_proxy.initiate_chat(
         chatbot,
         message="genere un post",
