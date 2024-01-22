6249d7be94e3c52e34ddd04ebce7309f78b5451b
Richenel2 <yvanprogramme@gmail.com>
2024-01-22 18:42:48 +0100

    sas

--- api.py
+++ api.py
@@ -2,11 +2,9 @@ from flask import Flask
 from ai import execute
 from datetime import datetime
 
-from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
 
-scheduler = APScheduler()
 
 @app.route('/execute')
 def post_linkedin():
@@ -20,16 +18,9 @@ def hello():
     current_time = now.strftime("%H:%M:%S")
     return f"Current Time = {current_time}"
 
-@scheduler.task('interval', id='my_job', minute=40, hour = 17)
-def my_job():
-    execute()
-    print('This job is executed every 10 seconds.')
 
 
 if __name__ == '__main__':
 
-    scheduler.init_app(app)
-
-    scheduler.start()
 
     app.run()
